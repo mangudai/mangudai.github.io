3093f989bc01dfba2d844297c45bbf826232fd4f
Nikita Litvin <n@deltaidea.com>
2017-07-16 17:31:22 +0500

    feat(editor): add linting using Mangudai

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import { example } from './rms-example'
 import { language } from './rms-monarch-language'
+import { throttle } from 'lodash'
+import { parse, lint } from 'mangudai'
 
 monaco.languages.register({ id: 'aoe2-rms' })
 monaco.languages.setMonarchTokensProvider('aoe2-rms', language)
 
+const model = monaco.editor.createModel(example, 'aoe2-rms')
+model.onDidChangeContent(() => {
+  lintSometimes(model.getValue())
+})
+
+const lintSometimes = throttle((script: string) => {
+  const { ast, errors } = parse(script)
+  const textSpanErrors = ast ? lint(ast) : errors
+  monaco.editor.setModelMarkers(model, 'rms-owner', textSpanErrors.map(error => ({
+    severity: monaco.Severity.Error,
+    message: error.message,
+    startLineNumber: error.boundaries.start.line,
+    startColumn: error.boundaries.start.col + 1,
+    endLineNumber: error.boundaries.end.line,
+    endColumn: error.boundaries.end.col + 1
+  })))
+}, 300, { leading: false, trailing: true })
+
 monaco.editor.create(document.querySelector('.editor') as HTMLElement, {
-  language: 'aoe2-rms',
+  model: model,
   roundedSelection: false,
   scrollBeyondLastLine: false,
+  cursorBlinking: 'smooth',
   wordWrap: 'on',
-  theme: 'vs-dark',
-  value: example
+  theme: 'vs-dark'
 })
